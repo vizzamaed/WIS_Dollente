018da7a5408b06d8f3d35e89c2b535e78494be40
vizzamaed <vizzamaedollente@gmail.com>
2024-01-13 22:48:17 +0800

    PHP Syntax Overview

--- learning_php.php
+++ learning_php.php
@@ -6,6 +6,32 @@
     <title>Learning PHP</title>
 </head>
 <body>
+    <?php
+    //PHP Syntax Overview
+    $age = 2024 - 2000;
+    $greetings="Hi!, ";
+    $name= "Vizza Mae";
+    print("$greetings $name<br>");
+    print("You are $age years old.<br>");
+
+    if($age > 17)
+    {
+        print ("You passed the minimum age requirement!<br>");
+    }
+    else
+    {
+        print("Sorry, your age is not allowed<br>");
+    }
+
+    if(2+3 == 5)
+    {
+        print("Just Practicing!");
+    }
+    else
+    {
+        print("This must not show!")
+    }
+    ?>
     
 </body>
 </html>
