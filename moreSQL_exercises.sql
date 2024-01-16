c8a8d03e4f01a3b7290ee57af0625a7d2288bf51
vizzamaed <vizzamaedollente@gmail.com>
2024-01-16 17:46:58 +0800

    More SQL Exercises

--- moreSQL_exercises.sql
+++ moreSQL_exercises.sql
@@ -13,3 +13,21 @@ create table Course(
     CourseName varchar(255),
     Credits varchar(255)
 );
+
+create table Instructor(
+    InstructorID int Primary Key,
+    Firstname varchar(255),
+    Lastname varchar (255),
+    Email varchar(255),
+    Phone int NOT NULL
+);
+
+create table Enrollment(
+    Enrollment int Primary Key,
+    StudentID int,
+    Foreign Key (StudentID) References Student(StudentID),
+    CourseID int,
+    Foreign Key (CourseID) References Course (CourseID),
+    EnrollmentDate DATE,
+    Grade int NOT NULL
+);
