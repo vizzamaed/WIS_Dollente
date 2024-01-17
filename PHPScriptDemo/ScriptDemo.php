41f143768fd6815bd421721f32acc6f44a25b02a
vizzamaed <vizzamaedollente@gmail.com>
2024-01-17 23:02:41 +0800

    PHP Script Demo

--- PHPScriptDemo/ScriptDemo.php
+++ PHPScriptDemo/ScriptDemo.php
@@ -20,7 +20,7 @@ if($result){
     //Process the results
     while ($row = $result->fetch_assoc()){
         echo "Student ID: " . $row["StudentID"]." - Firstname: " . $row["Firstname"].
-        " - Lastname: " . $row["Lastname"]. " - Date of Birth" . $row["DateOfBirth"].
+        " - Lastname: " . $row["Lastname"]. " - Date of Birth: " . $row["DateOfBirth"].
         " - Email: " . $row["Email"]. " - Phone: " . $row["Phone"]."<br>";
     }
 }else{
@@ -35,6 +35,20 @@ if ($result){
     while ($row=$result->fetch_assoc()){
         echo "Course ID: " . $row["CourseID"]. " - CourseName: " . $row["CourseName"]."",
         " - Credits: ". $row["Credits"]. "<br>";
+    }
+}else{
+    echo "Error: ".$sql."<br>".$conn->error;
+}
+
+$sql = "SELECT * FROM Enrollment";
+$result=$conn->query($sql);
+
+if ($result){
+    //Process the results
+    while ($row=$result->fetch_assoc()){
+        echo "Enrollment ID: ".$row["EnrollmentID"]." - StudentID: ".$row["StudentID"]. 
+        " - CourseID: " .$row["CourseID"]." - Enrollment Date: " .$row["EnrollmentDate"]. 
+        " - Grade: " .$row["Grade"]."<br>";
     }
 }else{
     echo "Error: ".$sql."<br>".$conn->error;
